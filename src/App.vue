e957d5bebb43eb78856372842d00fccb2b2d5acc
grgr <grigorizaika@outlook.com>
2019-05-28 00:30:34 +0200

    Login form. No logic.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,70 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <login-form />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LoginForm from './components/Login.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
-}
+    LoginForm
+  },
+  data: () => ({
+    message: "wzhhhh",
+  }),
+};
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=Poppins|Righteous&display=swap');
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Poppins', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.bumblebee-yellow {
+  color: #ffb101;
+}
+
+button {
+    width: 250px;
+    height: 40px;
+    border-radius: 36px;
+    margin: 4px;
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: normal;
+    font-size: 14px;
+    color: #ffffff;
+
+    border-width: 0px;
+  }
+
+input[type='text'] { /* Chrome, Firefox, Opera, Safari 10.1+ */
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: normal;
+    font-size: 18px;
+    opacity: 0.9; /* Firefox */
+    padding-left: 32px;
+
+    border-width: 1px;
+  }
+
+::placeholder { /* Chrome, Firefox, Opera, Safari 10.1+ */
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: normal;
+    font-size: 18px;
+    opacity: 0.5; /* Firefox */
+
 }
 </style>
---
+++ src/components/Login.vue
@@ -0,0 +1,71 @@
+<template>
+    <section id="login-section" class="form">
+        <h3 class="tapstr-logo bumblebee-yellow">tapstr.</h3>
+        <input class="tapstr-input" type="text" name="femail" placeholder="Email"><br/>
+        <input class="tapstr-input" type="text" name="fassword" placeholder="Password"><br/>
+        <p style="color: #ffffff;">Don't have an account?</p>
+        <p><a class="bumblebee-yellow">Sign Up</a></p>
+        <p style="color: #ffffff;">OR</p>
+        <button class="colors-facebook" type="button">Login using Facebook</button><br/>
+        <button class="colors-instagram" type="button">Login using Instagram</button><br/>
+
+        <p style="color: #ffffff; font-size: 12px;">Forgot password?</p>
+
+
+    </section>
+</template>
+
+
+<script>
+export default {
+  name: 'LoginForm',
+  data: () => ({
+    visible: true,
+
+  }),
+};
+</script>
+
+
+<style>
+/* These imports should be in a single main file */
+@import url('https://fonts.googleapis.com/css?family=Righteous&display=swap');
+
+#login-section {
+    background: rgba(0, 0, 0, .5) url('../assets/images/jakub-kapusnak-296881-unsplash-min.jpg');
+    background-blend-mode: overlay;
+    height: 500px; /* You must set a specified height */
+    background-position: center; /* Center the image */
+    background-repeat: no-repeat; /* Do not repeat the image */
+    background-size: cover; /* Resize the background image to cover the entire container*/
+    filter: blur(4);
+}
+
+.tapstr-logo {
+    font-family: 'Righteous', cursive;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 48px;
+    line-height: 60px;
+
+    align-items: center;
+    text-align: center;
+}
+
+.tapstr-input {
+    width: 300px;
+    height: 40px;
+    border-radius: 36px;
+    margin: 8px;
+}
+
+.colors-facebook {
+    background: #3b5998;
+    color: #ffffff;
+}
+
+.colors-instagram {
+    background: #833ab4;
+    color: #ffffff;
+}
+</style>
